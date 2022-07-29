f508bef3e85367b3e845019c2b27a33a55633063
Matthew-McDowell <justin.mcdowell@gmail.com>
2022-07-29 13:26:01 -0500

    7/28/22 Work

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -2,8 +2,6 @@ from socket import fromshare
 from django import forms
 from .models import Post, Category
 
-
-#choices = [('Uncategorized', 'Uncategorized'), ('coding', 'coding'), ('Gaming', 'Gaming'), ('Weightlifting', 'Weightlifting')]
 choices = Category.objects.all().values_list('name', 'name')
 
 choice_list = []
@@ -14,13 +12,12 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id':'user', 'type':'hidden'}),
-            #'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
