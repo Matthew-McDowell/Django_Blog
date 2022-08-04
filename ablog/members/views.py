010a871a40cff74b9267454c43918b8a94c18e6a
Matthew-McDowell <justin.mcdowell@gmail.com>
2022-08-04 12:35:31 -0500

    Changes made while working

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,10 +1,25 @@
 from ast import Pass
-from django.shortcuts import render
+from cProfile import Profile
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from theblog.models import Profile
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        
+        context["page_user"] = page_user
+        return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
